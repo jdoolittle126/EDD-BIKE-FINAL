b18c2c77422694cb487bd09a2d1b1ec7e1fbdca2
jdoolittle126 <jdoolittle126@gmail.com>
2018-05-17 21:30:51 -0400

    hard day on the farm

--- BIKE-EDD/src/chris/chace/jon/Controller.java
+++ BIKE-EDD/src/chris/chace/jon/Controller.java
@@ -2,42 +2,42 @@ package chris.chace.jon;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.Pin;
-import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class Controller {
-	// - IN MILLIS - \\
-	//02 and 03
-	
-	
-	public static final int THREAD_SLEEP_LENGTH = 500, BLINK_DURATION = 500;
-	
-	public static final Pin[] PINS = RaspiPin.allPins();
-	public static final int PIN_DIRECTIONAL_LEFT = 02, PIN_DIRECTIONAL_RIGHT = 03;
-	final static GpioController gpio = GpioFactory.getInstance();
 
-	public static void main(String[] args) throws InterruptedException {
-		
-		final GpioPinDigitalInput button = gpio.provisionDigitalInputPin(Controller.PINS[02],
-				PinPullResistance.PULL_DOWN);
-		button.addListener(new GpioPinListenerDigital() {
-			@Override
-			public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-				if (event.getState().isHigh()) {
-					//THING
-					gpio.shutdown();
-				}
-			}
-		});
-		
+	static final int THREAD_SLEEP_LENGTH = 100, DIRECTIONAL_DURATION = 400, DIRECTIONAL_LIFESPAN = 15000;
+	static int PIN_TRIGGER_DIRECTIONAL_LEFT = 02, PIN_TRIGGER_DIRECTIONAL_RIGHT = 04, PIN_DIRECTIONAL_LEFT = 03, PIN_DIRECTIONAL_RIGHT = 05;
+	static Pin[] PINS;
+	static GpioController gpio;
+	static boolean running;
+
+	static {
+		PINS = RaspiPin.allPins();
+		gpio = GpioFactory.getInstance();
+		running = true;
 	}
 	
-	public static void blink(int pin) {
-		gpio.provisionDigitalOutputPin(Controller.PINS[pin]).blink(Controller.BLINK_DURATION);
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("DEBUG: Starting bike program");
+		System.out.println(PINS);
+		if(args.length > 0) {
+			PIN_TRIGGER_DIRECTIONAL_LEFT = Integer.valueOf(args[0]);
+			PIN_TRIGGER_DIRECTIONAL_RIGHT = Integer.valueOf(args[1]);
+			PIN_DIRECTIONAL_LEFT = Integer.valueOf(args[2]);
+			PIN_DIRECTIONAL_RIGHT = Integer.valueOf(args[3]);
+		}
+		
+		Directional.init();
+		
+		while(running) {
+			Thread.sleep(Controller.THREAD_SLEEP_LENGTH);
+		}
+		
+		gpio.shutdown();
+		System.out.println("Program Termininated");
+
 	}
 	
 }
---
+++ BIKE-EDD/src/chris/chace/jon/Directional.java
@@ -0,0 +1,76 @@
+package chris.chace.jon;
+
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+
+public class Directional {
+	private static boolean left, right;
+	private static GpioPinDigitalInput PIN_BUTTON_LEFT, PIN_BUTTON_RIGHT;
+	private static GpioPinDigitalOutput PIN_LEFT, PIN_RIGHT;
+
+	public static void init() {
+		System.out.println("DEBUG: Directional initiated");
+		PIN_BUTTON_LEFT = Controller.gpio.provisionDigitalInputPin(Controller.PINS[Controller.PIN_TRIGGER_DIRECTIONAL_LEFT], PinPullResistance.PULL_DOWN);
+		PIN_BUTTON_RIGHT = Controller.gpio.provisionDigitalInputPin(Controller.PINS[Controller.PIN_TRIGGER_DIRECTIONAL_RIGHT],PinPullResistance.PULL_DOWN);
+		PIN_LEFT = Controller.gpio.provisionDigitalOutputPin(Controller.PINS[Controller.PIN_DIRECTIONAL_LEFT]);
+		PIN_RIGHT = Controller.gpio.provisionDigitalOutputPin(Controller.PINS[Controller.PIN_DIRECTIONAL_RIGHT]);
+		
+		
+		PIN_BUTTON_LEFT.addListener(new GpioPinListenerDigital() {
+					@Override
+					public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+						if (event.getState().isHigh())
+							left();
+					}
+				});
+
+		PIN_BUTTON_RIGHT.addListener(new GpioPinListenerDigital() {
+					@Override
+					public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+						if (event.getState().isHigh())
+							right();
+					}
+				});
+	}
+
+	private static void off() {
+		left = right = false;
+		//PIN_LEFT.setState(false);
+		//PIN_RIGHT.setState(false);
+	}
+
+	private static void left() {
+		System.out.println("DEBUG: Left directional triggered");
+		if (right) {
+			off();
+			left = true;
+			PIN_LEFT.blink(Controller.DIRECTIONAL_DURATION, Controller.DIRECTIONAL_LIFESPAN);
+		} else if (left)
+			off();
+		else {
+			left = true;
+			PIN_LEFT.blink(Controller.DIRECTIONAL_DURATION, Controller.DIRECTIONAL_LIFESPAN);
+		}
+					
+	
+	}
+
+	private static void right() {
+		System.out.println("DEBUG: Right directional triggered");
+		if (left) {
+			off();
+			right = true;
+			PIN_RIGHT.blink(Controller.DIRECTIONAL_DURATION, Controller.DIRECTIONAL_LIFESPAN);
+		} else if (right)
+			off();
+		else {
+			right = true;
+			PIN_RIGHT.blink(Controller.DIRECTIONAL_DURATION, Controller.DIRECTIONAL_LIFESPAN);
+		}
+			
+	}
+
+}
